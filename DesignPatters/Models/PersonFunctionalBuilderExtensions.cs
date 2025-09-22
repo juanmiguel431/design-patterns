9c6f21a624fd1f10c01d9dfe508d92af2fa6389c
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2025-09-21 22:04:49 -0400

    PersonFunctionalBuilderExtensions in a separate file.

---
+++ DesignPatters/Models/PersonFunctionalBuilderExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPatters.Models;
+
+public static class PersonFunctionalBuilderExtensions
+{
+    public static PersonFunctionalBuilder AddFullName(this PersonFunctionalBuilder builder, string firstName, string middleName, string lastName)
+    {
+        return builder.SetFirstName(firstName)
+            .SetMiddleName(middleName)
+            .SetLastName(lastName);
+    }
+}
