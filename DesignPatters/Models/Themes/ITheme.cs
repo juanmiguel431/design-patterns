cdfb562c81d6c2c8f8a99f768d1e4888a520fc83
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2025-09-24 12:01:45 -0400

    TrackingThemeFactory is a separte file.

--- DesignPatters/Models/Themes/ITheme.cs
+++ DesignPatters/Models/Themes/ITheme.cs
@@ -1,39 +1,7 @@
-﻿using System.Text;
-
-namespace DesignPatters.Models.Themes;
+﻿namespace DesignPatters.Models.Themes;
 
 public interface ITheme
 {
     string TextColor { get; }
     string BrgColor { get; }
-}
-
-public class TrackingThemeFactory
-{
-    private readonly List<WeakReference<ITheme>> _themes = new();
-    
-    public ITheme CreateTheme(bool dark = false)
-    {
-        ITheme theme = dark ? new DarkTheme() : new LightTheme();
-        _themes.Add(new WeakReference<ITheme>(theme));
-        return theme;
-    }
-
-    public string Info
-    {
-        get
-        {
-            var sb = new StringBuilder();
-            foreach (var reference in _themes)
-            {
-                if (reference.TryGetTarget(out var theme))
-                {
-                    var isDark = theme is DarkTheme;
-                    sb.Append(isDark ? "Dark" : "Light")
-                        .AppendLine(" theme");
-                }
-            }
-            return sb.ToString();
-        }
-    }
 }
