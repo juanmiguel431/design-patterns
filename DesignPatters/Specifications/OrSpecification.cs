28675d6f34344059a7a8bee68e4e078944c314bd
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2025-09-29 08:59:45 -0400

    OrSpecification

---
+++ DesignPatters/Specifications/OrSpecification.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPatters.Specifications;
+
+public class OrSpecification<T> : Specification<T>
+{
+    private readonly ISpecification<T> _left, _right;
+
+    public OrSpecification(ISpecification<T> left, ISpecification<T> right)
+    {
+        _left = left;
+        _right = right;
+    }
+    
+    public override bool IsSatisfiedBy(T item)
+    {
+        return _left.IsSatisfiedBy(item) || _right.IsSatisfiedBy(item);
+    }
+}
