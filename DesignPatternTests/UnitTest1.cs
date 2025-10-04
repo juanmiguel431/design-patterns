c034a9c16b173afb832e1778e00f1ab0768a87f9
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2025-10-04 16:16:54 -0400

    Flyweight pattern.

--- DesignPatternTests/UnitTest1.cs
+++ DesignPatternTests/UnitTest1.cs
@@ -1,15 +1,78 @@
-﻿namespace DesignPatternTests;
+﻿using DesignPatters.Models.Flyweight;
+using JetBrains.dotMemoryUnit;
+
+namespace DesignPatternTests;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
 
     [Test]
-    public void Test1()
+    public void Test()
     {
-        Assert.Pass();
+        var f = "Juan Miguel";
+        
+        var f2 = new string(f.ToCharArray());
+        
+        var same = object.ReferenceEquals(f, f2);
+        
+        Assert.That(f, Is.SameAs(f2));
+    }
+
+
+    [Test]
+    public void TestUser() // 31,286,846 vs 23,330,166
+    {
+        var firstNames = Enumerable.Range(0, 500).Select(x => RandomString()).ToList();
+        var lastNames = firstNames.Select(x => new string(x.ToCharArray())).ToList();
+        
+        var users = new List<User>();
+        foreach (var firstName in firstNames)
+        foreach (var lastName in lastNames)
+            users.Add(new User($"{firstName} {lastName}"));
+        
+        ForceGC();
+
+        dotMemory.Check(memory =>
+        {
+            Console.WriteLine(memory.SizeInBytes);
+        });
+    }
+    
+    [Test]
+    public void TestUser2() // 23,330,166 vs 31,286,846
+    {
+        var firstNames = Enumerable.Range(0, 500).Select(x => RandomString()).ToList();
+        var lastNames = firstNames.Select(x => new string(x.ToCharArray())).ToList();
+        
+        var users = new List<User2>();
+        foreach (var firstName in firstNames)
+        foreach (var lastName in lastNames)
+            users.Add(new User2($"{firstName} {lastName}"));
+        
+        ForceGC();
+
+        dotMemory.Check(memory =>
+        {
+            Console.WriteLine(memory.SizeInBytes);
+        });
+    }
+
+    private void ForceGC()
+    {
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        GC.Collect();
+    }
+
+    private static string RandomString()
+    {
+        var random = new Random();
+        var randomNumbers = Enumerable.Range(0, 10).Select(i =>
+        {
+            var randomNumber = 'a' + random.Next(26);
+            return (char)randomNumber;
+        }).ToArray();
+        
+        return new string(randomNumbers);
     }
 }
---
+++ DesignPatters/Models/Flyweight/User.cs
@@ -0,0 +1,33 @@
+﻿namespace DesignPatters.Models.Flyweight;
+
+public class User
+{
+    public string FullName { get; set; }
+    
+    public User(string fullName)
+    {
+        FullName = fullName ?? throw new ArgumentNullException(nameof(fullName));
+    }
+}
+
+public class User2
+{
+    private static readonly List<string> Strings = new();
+    private readonly int[] _names;
+
+    public User2(string fullName)
+    {
+        _names = fullName.Split(' ').Select(GetOrAdd).ToArray();
+    }
+
+    private static int GetOrAdd(string s)
+    {
+        var index = Strings.IndexOf(s);
+        if (index != -1) return index;
+            
+        Strings.Add(s);
+        return Strings.Count - 1;
+    }
+    
+    public string FullName => string.Join(' ', _names.Select(i => Strings[i]).ToArray());
+}
