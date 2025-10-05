93d4c7e1f23493a1ff2a38e2b90dbf95aac6dcd4
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2025-10-05 10:18:20 -0400

    Composite Proxy With array-backed properties - The problem. Fragil code.

---
+++ DesignPatters/Models/Proxy/MasonrySettings.cs
@@ -0,0 +1,23 @@
+﻿namespace DesignPatters.Models.Proxy;
+
+public class MasonrySettings
+{
+    public bool? All
+    {
+        get
+        {
+            if (Pillars && Walls && Floors) return Pillars;
+            return null;
+        }
+        set
+        {
+            if (!value.HasValue) return;
+            Pillars = Walls = Floors = value.Value;
+        }
+    }
+    
+    
+    public bool Pillars { get; set; }
+    public bool Walls { get; set; }
+    public bool Floors { get; set; }
+}
