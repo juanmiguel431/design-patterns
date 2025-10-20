241e7783595571ae9872a1aa0b153ece45e8f2a8
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2025-10-19 21:50:40 -0400

    Visitor Pattern - with reflection

--- DesignPatters/Models/Visitor/Expression.cs
+++ DesignPatters/Models/Visitor/Expression.cs
@@ -2,53 +2,8 @@
 
 namespace DesignPatters.Models.Visitor;
 
-using DictType = Dictionary<Type, Action<Expression, StringBuilder>>;
-
 public abstract class Expression
 {
     public abstract void Print(StringBuilder sb);
     public abstract void Accept(IExpressionVisitor visitor);
-}
-
-public static class ExpressionPrinter
-{
-    private static readonly DictType Actions = new()
-    {
-        [typeof(DoubleExpression)] = (e, sb) =>
-        {
-            var de = (DoubleExpression)e;
-            sb.Append(de.Value);
-        },
-        [typeof(AdditionExpression)] = (e, sb) =>
-        {
-            var additionExpression = (AdditionExpression)e;
-            
-            sb.Append('(');
-            additionExpression.Left.PrintFromExtensionV2(sb);
-            sb.Append('+');
-            additionExpression.Right.PrintFromExtensionV2(sb);
-            sb.Append(')');
-        }
-    };
-
-    public static void PrintFromExtensionV2(this Expression e, StringBuilder sb)
-    {
-        Actions[e.GetType()](e, sb);
-    }
-    
-    public static void PrintFromExtension(this Expression e, StringBuilder sb)
-    {
-        if (e is DoubleExpression de)
-        {
-            sb.Append(de.Value);
-        }
-        else if (e is AdditionExpression ae)
-        {
-            sb.Append('(');
-            ae.Left.Print(sb);
-            sb.Append('+');
-            ae.Right.Print(sb);
-            sb.Append(')');
-        }
-    }
 }
---
+++ DesignPatters/Models/Visitor/ExpressionPrinter.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace DesignPatters.Models.Visitor;
+
+public static class ExpressionPrinter
+{
+    private static readonly Dictionary<Type, Action<Expression, StringBuilder>> Actions = new()
+    {
+        [typeof(DoubleExpression)] = (e, sb) =>
+        {
+            var de = (DoubleExpression)e;
+            sb.Append(de.Value);
+        },
+        [typeof(AdditionExpression)] = (e, sb) =>
+        {
+            var additionExpression = (AdditionExpression)e;
+            
+            sb.Append('(');
+            additionExpression.Left.PrintFromExtensionV2(sb);
+            sb.Append('+');
+            additionExpression.Right.PrintFromExtensionV2(sb);
+            sb.Append(')');
+        }
+    };
+
+    public static void PrintFromExtensionV2(this Expression e, StringBuilder sb)
+    {
+        Actions[e.GetType()](e, sb);
+    }
+    
+    public static void PrintFromExtension(this Expression e, StringBuilder sb)
+    {
+        if (e is DoubleExpression de)
+        {
+            sb.Append(de.Value);
+        }
+        else if (e is AdditionExpression ae)
+        {
+            sb.Append('(');
+            ae.Left.Print(sb);
+            sb.Append('+');
+            ae.Right.Print(sb);
+            sb.Append(')');
+        }
+    }
+}
